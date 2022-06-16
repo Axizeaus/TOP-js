0028621bbb66fc003687096b80bb57cc2ef8710c
Axizeaus <axizeaus@gmail.com>
2022-06-16 22:21:21 +0630

    password validation basic

---
+++ project-signup-form/script.js
@@ -0,0 +1,9 @@
+function validate(){
+    let pw = document.getElementById('pw');
+    let confirm_pw = document.getElementById('confirm-pw');
+    if(pw.value === confirm_pw.value){
+        return;
+    } else {
+        alert('password not same')
+    }
+}
