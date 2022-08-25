466ddccc94ea19ce8ed3cc66d302588a49637e6e
Axizeaus <axizeaus@gmail.com>
2022-08-25 22:03:58 +0630

    species

---
+++ classes/species.js
@@ -0,0 +1,10 @@
+class MyArray extends Array{
+    static get [Symbol.species]() {return Array;}
+}
+
+const a = new MyArray(1,2,3);
+const mapped = a.map((x) => x * x);
+
+console.log(a, mapped);
+console.log(mapped instanceof MyArray);
+console.log(mapped instanceof Array);
