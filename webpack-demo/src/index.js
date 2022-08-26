c9dc4b4a5b3f24632abadd86c874fae9ed980172
Axizeaus <axizeaus@gmail.com>
2022-08-27 03:23:04 +0630

    js modules done

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,12 +1,16 @@
-import _ from 'lodash';
+// import _ from 'lodash';
+import myName from './myName';
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
+    element.textContent = myName("Ye Man Oo");
     return element;
+  
+    // return element;
   }
   
   document.body.appendChild(component());
