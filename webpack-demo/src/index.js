a2506fa906ab24356d28f3af30030a1554b3be1d
Axizeaus <axizeaus@gmail.com>
2022-08-27 23:24:39 +0630

    Loading images complete

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,18 +1,27 @@
-import _ from 'lodash';
-import './style.css';
+import _ from "lodash";
+import "./style.css";
+import Icon from "./icon.png";
 // import myName from './myName';
 
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    // element.textContent = myName("Ye Man Oo");
-    // return element;
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const element = document.createElement("div");
+
+  // Lodash now imported by this script
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  // element.textContent = myName("Ye Man Oo");
+  // return element;
+
+  // Add the image to our existing div.
+
+  const myIcon = new Image();
+
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  return element;
+}
+
+document.body.appendChild(component());
