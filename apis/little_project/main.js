109f10fd547d5280c6b59b78fc438207cfd9e4c4
Axizeaus <axizeaus@gmail.com>
2023-03-20 15:36:21 +0630

    async await lesson finished

--- apis/little_project/main.js
+++ apis/little_project/main.js
@@ -2,21 +2,27 @@ const img = document.querySelector('img');
 const searchBar = document.getElementById('searchbar');
 const btn = document.getElementById('btn');
 
-btn.addEventListener('click', getDataFromSearchBar)
+btn.addEventListener('click', getDataFromSearchBar);
+window.onload = () =>{
+  changeGif('Welcome');
+}
 
-function changeGif(keyWord){
-  img.src = '#'
-  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Xp8HAklJWvCKqA1IXFoBQwtSwtTZCS8e&s=${keyWord}`, {mode: 'cors'})
-  .then(response => response.json())
-  .then(response => img.src = response.data.images.original.url)
-  .catch(e => console.log(e));
+async function changeGif(keyWord){
+  try{
+    let response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Xp8HAklJWvCKqA1IXFoBQwtSwtTZCS8e&s=${keyWord}`, {mode: 'cors'})
+    let responseData = await response.json();
+    img.src = responseData.data.images.original.url;
+  } catch {
+    console.log(e);
+  }
 }
 
 function getDataFromSearchBar(){
   const data = searchBar.value;
-  if (data === null || data === undefined || data === ''){
+  if (!data){
     changeGif('nothing');
-  } else {
+  }
+  else {
     changeGif(data);
   }
 }
