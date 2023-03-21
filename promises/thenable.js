e1c040ac32fa602a0f912e2dd1e5e76575f484c6
Axizeaus <axizeaus@gmail.com>
2023-03-21 15:19:23 +0630

    finished async await section"

---
+++ promises/thenable.js
@@ -0,0 +1,18 @@
+class Thenable{
+  constructor(num){
+    this.num = num;
+  }
+
+  then(resolve, reject){
+    console.log(resolve);
+    setTimeout(()=>{
+      resolve(this.num * 2) 
+    }, 1000)
+  }
+}
+
+new Promise(resolve => resolve(1))
+.then(result => {
+  return new Thenable(result);
+})
+.then(console.log)
